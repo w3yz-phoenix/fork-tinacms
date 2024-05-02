2d9ca5557c1a08671a95b58fe5ff8ffc54f54e5b
Yasin Uslu <nepjua@gmail.com>
2024-05-02 13:18:29 +0300

    Configure tina

--- apps/storefront/next.config.mjs
+++ apps/storefront/next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+      },
+      {
+        source: "/admin",
+        destination: "/admin/index.html",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
