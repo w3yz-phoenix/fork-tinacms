6dae3c2b3b81c323ae5ae1a228562e9e2e392a96
Yasin Uslu <nepjua@gmail.com>
2024-05-20 13:22:53 +0300

    Update dev config

--- ops/scripts/generate-dev-env.sh
+++ ops/scripts/generate-dev-env.sh
@@ -1,28 +1,50 @@
 #!/usr/bin/env bash
 
-export SHOP_DOMAIN="${NEXT_PUBLIC_SHOP_NAME}.${NEXT_PUBLIC_ROOT_DOMAIN}"
-export NAMESPACE="w3yz-shop-${NEXT_PUBLIC_SHOP_NAME}"
-MY_PERSONAL_GITHUB_TOKEN="$(gh auth token)"
-export MY_PERSONAL_GITHUB_TOKEN="${MY_PERSONAL_GITHUB_TOKEN}"
-NEXT_PUBLIC_MONGO_PORT="$(kubectl get -n "${NAMESPACE}" service ferretdb-exposed -o json | jq '.spec.ports[0].nodePort')"
-export NEXT_PUBLIC_MONGO_PORT="${NEXT_PUBLIC_MONGO_PORT}"
+# Generates a .env file for development based on the current k8s cluster state
+# If the user is not logged in to GitHub, it ignores the GITHUB_PERSONAL_ACCESS_TOKEN
+# If the user does not have any kubectl context, it will ignore the MONGODB_URI
 
-export NODE_ENV=development
-export NEXT_PUBLIC_URL="https://localhost:3000/"
-export NEXT_PUBLIC_CMS_BASE_URL="http://localhost:3000/"
-export NEXT_PUBLIC_ECOM_API_URL="https://api.${NEXT_PUBLIC_SHOP_NAME}.${NEXT_PUBLIC_ROOT_DOMAIN}/graphql/"
-export NEXT_PUBLIC_ECOM_NAME="${NEXT_PUBLIC_SHOP_NAME}"
-export NEXT_PUBLIC_SHOP_DOMAIN="${SHOP_DOMAIN}"
-export NEXTAUTH_SECRET="change-me"
-export MONGODB_URI="mongodb://${SHOP_DOMAIN}:${NEXT_PUBLIC_MONGO_PORT}/${NEXT_PUBLIC_SHOP_NAME}"
-export GITHUB_OWNER=w3yz-phoenix
-export GITHUB_REPO=live
-export GITHUB_BRANCH=main
-export GITHUB_PERSONAL_ACCESS_TOKEN="${MY_PERSONAL_GITHUB_TOKEN}"
-export TINA_PUBLIC_IS_LOCAL="false"
-export NEXT_PUBLIC_TINA_IS_LOCAL="false"
+generateDevEnv() {
+  export NODE_ENV=development
 
-cat << EOF > .env
+  export SHOP_DOMAIN="${NEXT_PUBLIC_SHOP_NAME}.${NEXT_PUBLIC_ROOT_DOMAIN}"
+  export NAMESPACE="w3yz-shop-${NEXT_PUBLIC_SHOP_NAME}"
+
+  local nodeName=$(kubectl get nodes -o json 2> /dev/null | jq '.items[0].metadata.name')
+  local githubToken=$(gh auth token 2> /dev/null)
+
+  if [ -z "$githubToken" ]; then
+    echo "No GitHub token found. Skipping GITHUB_PERSONAL_ACCESS_TOKEN"
+    export MY_PERSONAL_GITHUB_TOKEN=""
+  else
+    MY_PERSONAL_GITHUB_TOKEN="$(gh auth token)"
+    export MY_PERSONAL_GITHUB_TOKEN="${MY_PERSONAL_GITHUB_TOKEN}"
+  fi
+
+  if [ -z "$nodeName" ]; then
+    echo "No k8s context found. Skipping MONGODB_URI"
+    export NEXT_PUBLIC_MONGO_PORT=""
+    export MONGODB_URI=""
+  else
+    NEXT_PUBLIC_MONGO_PORT="$(kubectl get -n "${NAMESPACE}" service ferretdb-exposed -o json | jq '.spec.ports[0].nodePort')"
+    export NEXT_PUBLIC_MONGO_PORT="${NEXT_PUBLIC_MONGO_PORT}"
+    export MONGODB_URI="mongodb://${SHOP_DOMAIN}:${NEXT_PUBLIC_MONGO_PORT}/${NEXT_PUBLIC_SHOP_NAME}"
+  fi
+
+  export NEXT_PUBLIC_URL="https://localhost:3000/"
+  export NEXT_PUBLIC_CMS_BASE_URL="http://localhost:3000/"
+  export NEXT_PUBLIC_ECOM_API_URL="https://api.${NEXT_PUBLIC_SHOP_NAME}.${NEXT_PUBLIC_ROOT_DOMAIN}/graphql/"
+  export NEXT_PUBLIC_ECOM_NAME="${NEXT_PUBLIC_SHOP_NAME}"
+  export NEXT_PUBLIC_SHOP_DOMAIN="${SHOP_DOMAIN}"
+  export NEXTAUTH_SECRET="change-me"
+  export GITHUB_OWNER=w3yz-phoenix
+  export GITHUB_REPO=live
+  export GITHUB_BRANCH=main
+  export GITHUB_PERSONAL_ACCESS_TOKEN="${MY_PERSONAL_GITHUB_TOKEN}"
+  export TINA_PUBLIC_IS_LOCAL="false"
+  export NEXT_PUBLIC_TINA_IS_LOCAL="false"
+
+  cat << EOF > .env
 export NEXT_PUBLIC_SHOP_NAME="${NEXT_PUBLIC_SHOP_NAME}"
 export NEXT_PUBLIC_ROOT_DOMAIN="${NEXT_PUBLIC_ROOT_DOMAIN}"
 
@@ -45,3 +67,6 @@ export GITHUB_PERSONAL_ACCESS_TOKEN="${GITHUB_PERSONAL_ACCESS_TOKEN}"
 export TINA_PUBLIC_IS_LOCAL="${TINA_PUBLIC_IS_LOCAL}"
 export NEXT_PUBLIC_TINA_IS_LOCAL="${NEXT_PUBLIC_TINA_IS_LOCAL}"
 EOF
+}
+
+generateDevEnv
