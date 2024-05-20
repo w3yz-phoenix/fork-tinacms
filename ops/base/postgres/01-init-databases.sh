7e6ee6bcb4774cba9dd1bbfb4c84ddbd46ea69cb
Yasin Uslu <nepjua@gmail.com>
2024-05-20 15:31:31 +0300

    Fix saleor

---
+++ ops/base/postgres/01-init-databases.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<- EOSQL
+    CREATE DATABASE ferretdb;
+    CREATE DATABASE saleor;
+EOSQL
+
+curl https://gist.githubusercontent.com/yasinuslu/753779df923b1ff70134abcd78ab99bf/raw/38b411b8a982fd12a6c963c874b2aa5fc637d438/gistfile1.txt -o /tmp/saleor-dump.sql
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname saleor < /tmp/saleor-dump.sql
