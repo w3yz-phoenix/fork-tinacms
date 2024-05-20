8b027dc69798514c0f81caa8ed8aab242adee528
Yasin Uslu <nepjua@gmail.com>
2024-05-20 15:43:28 +0300

    Update scripts

--- ops/base/postgres/01-init-databases.sh
+++ ops/base/postgres/01-init-databases.sh
@@ -8,4 +8,7 @@ EOSQL
 
 curl https://gist.githubusercontent.com/yasinuslu/753779df923b1ff70134abcd78ab99bf/raw/38b411b8a982fd12a6c963c874b2aa5fc637d438/gistfile1.txt -o /tmp/saleor-dump.sql
 
+# replace API_FQDN with $API_FQDN
+sed -i "s/API_FQDN/$API_FQDN/g" /tmp/saleor-dump.sql
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname saleor < /tmp/saleor-dump.sql
